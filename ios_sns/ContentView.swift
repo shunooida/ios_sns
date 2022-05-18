b50b187e968af3ef9a4b6a997b5a04e6d4e581ba
shunooida <gpu.shun@gmail.com>
2022-05-18 22:05:39 +0900

    Initial Commit

---
+++ ios_sns/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_sns
+//
+//  Created by 大井田駿 on 2022/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_sns/ios_snsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_snsApp.swift
+//  ios_sns
+//
+//  Created by 大井田駿 on 2022/05/18.
+//
+
+import SwiftUI
+
+@main
+struct ios_snsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
